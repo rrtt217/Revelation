f71b4cd55d3d050e061495fe2d8f4d5b95759ed4
rrtt217 <wwq169@mail.ustc.edu.cn>
2026-06-12 23:51:54 +0800

    Tweak skylight

--- shaders/lib/lighting/SSR.glsl
+++ shaders/lib/lighting/SSR.glsl
@@ -21,7 +21,7 @@ vec4 CalculateSpecularReflections(Material material, vec3 worldNormal, vec3 worl
 	vec4 reflection = vec4(0.0, 0.0, 0.0, FP16_MAX);
 	if (skylight > EPS && isEyeInWater == 0) {
 		vec3 skyRadiance = textureBicubic(skyEnvMapTex, saturate(ProjectCubemap(lightDir, 96.0))).rgb;
-		reflection.rgb = skyRadiance * smoothstep(0.3, 0.7, skylight);
+		reflection.rgb = skyRadiance * smoothstep(0.4, 0.8, skylight);
 	}
 
 	uint stepCount = uint(SSRT_MAX_SAMPLES * oms(material.roughness * 0.75));
